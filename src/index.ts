73d59774fe800cfa362d55226468f90519e12cd0
mcsdevv <mail@mcs.dev>
2024-01-16 10:09:59 +0000

    remove ip addresses

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ export default async function main() {
   // Get the URL from the clipboard
   const { text: urlDirty } = await Clipboard.read();
 
-  // Replace hxxps with https and [.] with .
+  const ipRegex =
+    /\[?\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b\]?/g;
+
   const urlClean = urlDirty
     .toLowerCase()
     .replace(/hxxps/g, "https")
     .replace(/hxxp/g, "http")
     .replace(/\[\.\]/g, ".")
     .replace(/\[dot\]/g, ".")
-    .replace(/\[\:\]/g, ":");
+    .replace(/\[\:\]/g, ":")
+    .replace(ipRegex, "");
 
   // Copy the cleaned URL back to the clipboard
   await Clipboard.paste(urlClean);
